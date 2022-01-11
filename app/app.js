cd9e8b4fd34d03761a7c3431ad89fc5b62794745
Rushi Patel <patel.rus30@gmail.com>
2022-01-11 17:36:07 +0000

    [fix] remove unnecessary code

--- app/app.js
+++ app/app.js
@@ -182,34 +182,6 @@ app.get('/logout', function (req, res) {
     res.redirect('/login');
 });
 
-// Create a route for testing the db
-app.get("/db_test", function (req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from games';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
-});
-
-// Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function (req, res) {
-    res.send("Goodbye world!");
-});
-
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function (req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
-    console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
-    res.send("Hello " + req.params.name);
-});
-
-
 // Start server on port 3000
 app.listen(3000, function () {
     console.log(`Server running at http://127.0.0.1:3000`);
